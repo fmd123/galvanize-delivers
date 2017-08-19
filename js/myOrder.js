70ca6457f69fc1c5cac9c983bf49b363b3223b4b
fmd123 <fmdavis6@gmail.com>
2017-08-19 16:20:40 -0600

    stylesheet removed. Begin JS work

---
+++ js/myOrder.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+$("#orderPizza").on("click", function(){
+  console.log("ORDER THE PIZZA");
+  $("#cartData").clone().appendTo("tbody");
+  // instead of doing a bunch of grabbing text and fussing around why not
+  //clone and append a complete tr element that is associated with each item?
+  //save the string to number stuff for just before subtotal
+});
+
+$("#orderRoyale").on("click", function(){
+  console.log("ORDER THE Royale");
+  $("#cartData").clone().appendTo("tbody");
+  // for (#cartItem in tbody:last-child)
+  $("#cartItem").text("Royale with Cheese");
+
+});
+
+$("#orderRibs").on("click", function(){
+  console.log("ORDER THE Ribs");
+  $("#cartData").clone().appendTo("tbody");
+});
+
+$("#orderIC").on("click", function(){
+  console.log("ORDER THE Ice Cream BiscuIt");
+  $("#cartData").clone().appendTo("tbody");
+});
+
+
+
+// WHEN DO I WANT to DO THE CALCULATIONS?
+//The .text() method cannot be used on form inputs or scripts. To set or get the text value of input or textarea elements, use the .val() method. To get the value of a script element, use the .html() method. This is from: http://api.jquery.com/text/
